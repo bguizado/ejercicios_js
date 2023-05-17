c8997c9af0129b60e9d4d4224f070f8345f1a139
bguizado <brando.guizado.87@gmail.com>
2023-05-16 23:28:23 -0500

    Ejercicio 12 Resuelto

---
+++ script.js
@@ -0,0 +1,7 @@
+// Dado un arreglo de números, utiliza el método filter para crear un nuevo arreglo que contenga solo los números mayores que 5.
+
+const array = [12, 4, 18, 7, 1, 14, 5, 20, 9, 3]
+
+const newArray = array.filter(elemento => elemento > 5)
+
+console.log (newArray)
