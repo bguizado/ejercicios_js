05e116d576f885ab1f936e828b70406c8f819db6
bguizado <brando.guizado.87@gmail.com>
2023-05-17 20:51:20 -0500

    Ejercicio 50 Resuelto

---
+++ script.js
@@ -0,0 +1,9 @@
+//Crea una función llamada convertirAMayusculas que tome una cadena como argumento y devuelva la cadena en mayúsculas.
+
+function convertirAMayusculas(cadena) {
+    return cadena.toUpperCase();
+  }
+  
+  const texto = "Hola, mundo!";
+  const resultado = convertirAMayusculas(texto);
+  console.log(resultado); 
