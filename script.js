ed8345ab53d91458858ee31cde91d9d082826917
bguizado <brando.guizado.87@gmail.com>
2023-05-17 19:44:36 -0500

    Ejercicio 34 Resuelto

---
+++ script.js
@@ -0,0 +1,10 @@
+// Escribe una función que tome dos números como argumentos y devuelva el mayor de los dos.
+
+let numero = (a, b) => {
+    if(a > b){
+      return a
+    }else 
+      return b
+  };
+  
+  console.log(numero(6,3))
