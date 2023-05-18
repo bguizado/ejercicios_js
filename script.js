6c57a7c634efd4ad10ebebd2917bd77e6cb1ec11
bguizado <brando.guizado.87@gmail.com>
2023-05-17 20:31:46 -0500

    Ejercicio 44 Resuelto

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-// Dado un arreglo de palabras, utiliza el método map para crear un nuevo arreglo que contenga la primera letra de cada palabra.
+// Crea una función llamada calcularPromedio que tome un arreglo de números como argumento y devuelva el promedio de esos números.
 
-const array = ["manzana", "pera", "banana", "naranja", "sandía"];
-
-const primerasLetras = array.map(palabra => palabra.charAt(0));
-
-console.log(primerasLetras);
+function calcularPromedio(numeros) {
+    let suma = 0;
+    for (let i = 0; i < numeros.length; i++) {
+      suma += numeros[i];
+    }
+    return suma / numeros.length;
+  }
+  
+  const numeros = [5, 8, 12, 4, 9];
+  const promedio = calcularPromedio(numeros);
+  console.log(promedio);
