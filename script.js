e4b105b581b577166ddba2259ef63ce61d97425c
bguizado <brando.guizado.87@gmail.com>
2023-05-17 19:42:45 -0500

    Ejercicio 33 Resuelto

---
+++ script.js
@@ -0,0 +1,7 @@
+// Escribe un bucle que imprima los números impares del 1 al 50 en la consola.
+
+for (let numero = 0; numero <= 50; numero++){
+    if(numero%2 !== 0) {
+      console.log(numero);
+    }
+  }
