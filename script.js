91fe0e0c60886278d73d74b944a3afa4da4edbfe
bguizado <brando.guizado.87@gmail.com>
2023-05-17 20:56:55 -0500

    Ejercicio 51 Resuelto

---
+++ script.js
@@ -0,0 +1,9 @@
+// Crea una función llamada sumar que tome dos números como argumentos y devuelva la suma de esos números.
+
+function sumar(num1, num2) {
+    return num1 + num2;
+  }
+  
+  const resultado = sumar(5, 3);
+  console.log(resultado);
+  
