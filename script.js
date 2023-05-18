f5ed8aa96a6be4ed5d831f1a64fca39aba863592
bguizado <brando.guizado.87@gmail.com>
2023-05-17 19:30:00 -0500

    Ejercicio 27 Resuelto

---
+++ script.js
@@ -0,0 +1,7 @@
+// Dado un arreglo de números, utiliza el método map para crear un nuevo arreglo que contenga el doble de cada número.
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let arrayDoble = array.map(numero => numero * 2);
+
+console.log(arrayDoble);
